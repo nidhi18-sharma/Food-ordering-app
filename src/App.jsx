e8527e93683e37db1beda2314ee9bfd00e1953be
nidhi18-sharma <nidhis0418@gmail.com>
2026-06-06 00:28:02 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Components/Header.jsx";
+import Meals from "./Components/Meals.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Meals/>
+    </>
+  );
+}
+
+export default App;
