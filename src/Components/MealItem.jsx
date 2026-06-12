cb8e7aa86489bf474cf660c97c1377ba4e8f3d25
nidhi18-sharma <nidhis0418@gmail.com>
2026-06-12 14:56:07 +0530

    updates

--- src/Components/MealItem.jsx
+++ src/Components/MealItem.jsx
@@ -3,27 +3,36 @@ import Button from "./UI/Button.jsx";
 import { useContext } from "react";
 import CartContext from "../Store/CartContext.jsx";
 
-export default function MealItem({meal}){
-    const cartCtx=useContext(CartContext);
-function handleAddItem(){
-cartCtx.addItem(meal);
-}
+export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+  const cartItem = cartCtx.items.find((item) => item.id === meal.id);
+  function handleAddItem() {
+    cartCtx.addItem(meal);
+  }
 
-    return(
-        <li className="meal-item">
-<article>
-    <img src={`http://localhost:3000/${meal.image}`} alt={meal.name}/>
-   <div>
-    <h3>{meal.name}</h3>
-    <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
-    <p className="meal-item-description">{meal.description}</p>
-   </div>
-   <p className="meal-item-actions">
-    <Button onClick={handleAddItem}>Add to Cart</Button>
-   
-   </p>
-</article>
-        </li>
-
-    )
+  return (
+    <li className="meal-item">
+      <article>
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+        <div>
+          <h3>{meal.name}</h3>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
+          <p className="meal-item-description">{meal.description}</p>
+        </div>
+        <div className="meal-item-actions">
+          {cartItem ? (
+            <div className="meal-item-stepper">
+              <button onClick={() => cartCtx.removeItem(cartItem.id)}>-</button>
+              <span>{cartItem.quantity}</span>
+              <button onClick={() => cartCtx.addItem(cartItem)}>+</button>
+            </div>
+          ) : (
+            <Button onClick={handleAddItem}>Add to Cart</Button>
+          )}
+        </div>
+      </article>
+    </li>
+  );
 }
